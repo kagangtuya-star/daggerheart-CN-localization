901b74bda8ee6b87b4e54a5184661a871430de54
Nodra.Vr <nodra.vr@pm.me>
2025-08-28 12:36:06 +0200

    Add v2 style sheets

--- gulpfile.js
+++ gulpfile.js
@@ -6,28 +6,39 @@ const { spawn } = require('child_process');
 /*  Compile sass
 /* ----------------------------------------- */
 
-const SIMPLE_SASS = ['styles/**/*.scss'];
-function compileSass() {
-	return gulp.src('./styles/daggerheart.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('./styles'));
+const UO_SASS = ['styles/**/*.scss'];
+const DOCS_SASS = ['documentation/sass/**/*.scss'];
+
+function compileSassUOv1() {
+	return gulp
+		.src('./styles/src/daggerheart.v1.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./styles'));
+}
+function compileSassUOv2() {
+	return gulp
+		.src('./styles/src/daggerheart.v2.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./styles'));
 }
 
-const DOCS_SASS = ['documentation/sass/**/*.scss'];
-function compileDocSass() {
+function compileSassDocs() {
 	return gulp
 		.src('./documentation/sass/custom.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./documentation/docs/overrides/assets/stylesheets/'));
 }
+
 /* ----------------------------------------- */
 /*  Watch Updates
 /* ----------------------------------------- */
 
 function watchUpdates() {
-	gulp.watch(SIMPLE_SASS, compileSass);
+	gulp.watch(UO_SASS, gulp.series(compileSassUOv1, compileSassUOv2));
 }
 
 function watchDocs() {
-	gulp.watch(DOCS_SASS, compileDocSass);
+	gulp.watch(DOCS_SASS, compileSassDocs);
 }
 
 function mkdocs() {
@@ -50,7 +61,7 @@ function mkdocs() {
 /* ----------------------------------------- */
 
 module.exports = {
-	default: gulp.series(gulp.parallel(compileSass), watchUpdates),
-	css: compileSass,
-	docs: gulp.parallel(gulp.series(gulp.parallel(compileDocSass), watchDocs), mkdocs),
+	default: gulp.series(gulp.parallel(gulp.series(compileSassUOv1, compileSassUOv2)), watchUpdates),
+	css: gulp.series(compileSassUOv1, compileSassUOv2),
+	docs: gulp.parallel(gulp.series(gulp.parallel(gulp.series(compileSassUOv1, compileSassUOv2)), watchDocs), mkdocs),
 };
